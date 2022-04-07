37694bd5d9dce31bc7b0fb4a5c905b2f6eaa6cb0
Omer1 <omer.hadar96@gmail.com>
2022-04-07 14:38:49 +0300

    lower case

--- password.py
+++ password.py
@@ -1,4 +1,6 @@
 """ this program will check if the user insert available password """
 user_password = input("Please enter your password")
 len_test = None
+upper_case_test = None
+Lower_case_test = None
 
