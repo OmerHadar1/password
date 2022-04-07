fd645e7c14c76ec6c62fc446c62c9eae661c1fdf
Omer1 <omer.hadar96@gmail.com>
2022-04-07 14:54:11 +0300

    while

--- password.py
+++ password.py
@@ -1,6 +1,10 @@
 """ this program will check if the user insert available password """
-user_password = input("Please enter your password")
-len_test = None
+user_password = input("Please enter your password ")
+len_test = 10
 upper_case_test = None
 Lower_case_test = None
+digit_test = None
+repeat_test = None
+alpha_numeric_test = None
 
+while user_password < len_test:
